1d5dac3ed418229f1b7dba5b63e313c33c5e2386
Tyrn <you@example.com>
2018-06-23 22:23:52 +0300

    Unit tests added

--- lib/dart_procr.dart
+++ lib/dart_procr.dart
@@ -1,3 +1,54 @@
+import 'package:path/path.dart' as p;
+
+
+/// Returns true, if [path] has extension [ext], case and leading dot insensitive.
+///
+bool hasExtOf(String path, String ext) {
+  var e = ext == "" || ext[0] == "." ? ext : "." + ext;
+
+  return p.extension(path).toUpperCase() == e.toUpperCase();
+}
+
+
+/// Compares two lists of integers, string-style.
+///
+int arrayCmp(List<int> x, List<int> y) {
+  if(x.length == 0) return y.length == 0 ? 0 : -1;
+  if(y.length == 0) return x.length == 0 ? 0 : 1;
+  var i = 0;
+  for(; x[i] == y[i]; i++) {
+    if(i == x.length - 1 || i == y.length - 1) {
+      // The short array is a prefix of the long one; end reached. All is equal so far.
+      if(x.length == y.length) return 0;  // The long array is no longer than the short one.
+      return x.length < y.length ? -1 : 1;
+    }
+  }
+  // Difference encountered.
+  return x[i] < y[i] ? -1 : 1;
+}
+
+
+/// If both strings contain digits, returns numerical comparison based on the numeric
+/// values embedded in the strings, otherwise returns the standard string comparison.
+/// The idea of the natural sort as opposed to the standard lexicographic sort is one of coping
+/// with the possible absence of the leading zeros in 'numbers' of files or directories.
+///
+int strcmpNaturally(String x, String y) {
+  var a = strStripNumbers(x), b = strStripNumbers(y);
+
+  return a.length != 0 && b.length != 0 ? arrayCmp(a, b) : x.compareTo(y);
+}
+
+
+/// Returns a vector of integer numbers, embedded in [s].
+///
+List<int> strStripNumbers(String s) {
+  var num = new RegExp(r'\d+');
+
+  return num.allMatches(s).map((m) => int.parse(m[0])).toList();
+}
+
+
 /// Reduces comma delimited list of [authors] to initials.
 /// 
 String makeInitials(String authors) {
--- test/dart_procr_test.dart
+++ test/dart_procr_test.dart
@@ -2,17 +2,46 @@ import 'package:dart_procr/dart_procr.dart';
 import 'package:test/test.dart';
 
 void main() {
-  
-  test('Strings and lists of strings, review.', () {
-    expect(["alfa", "bravo"].join(", "), "alfa, bravo");
-    expect("щука"[0].toUpperCase(), "Щ");
-    expect('alfa, bravo'.split(','), ["alfa", " bravo"]);
-    expect("Kubler - -Ross".split(new RegExp(r'\s*(?:-\s*)+')), ["Kubler", "Ross"]);
-    expect('alfa"br"bravo"ch"charlie'
-                  .replaceAll(new RegExp(r'\"(?:\\.|[^\"\\])*\"'), " "),
-            "alfa bravo charlie");
-  });
 
+  test('padLeft: string padding.', () {
+    expect(3.toString().padLeft(5, "0"), "00003");
+    expect(15331.toString().padLeft(3, "0"), "15331");
+  });
+  
+  test('hasExtOf: checks extension.', () {
+    expect(hasExtOf("/alfa/bra.vo/charlie.ogg", "OGG"), true);
+    expect(hasExtOf("/alfa/bra.vo/charlie.ogg", ".ogg"), true);
+    expect(hasExtOf("/alfa/bra.vo/charlie.ogg", "mp3"), false);
+  });
+  
+  test('compareTo: compares strings, C-style.', () {
+    expect("alfa".compareTo("alfa"), 0);
+    expect("alfa".compareTo("bravo"), -1);
+  });
+  
+  test('arrayCmp: compares lists of integers, string-style.', () {
+    expect(arrayCmp([], []), 0);
+    expect(arrayCmp([1], []), 1);
+    expect(arrayCmp([3], []), 1);
+    expect(arrayCmp([1, 2, 3], [1, 2, 3, 4, 5]), -1);
+    expect(arrayCmp([1, 4], [1, 4, 16]), -1);
+    expect(arrayCmp([2, 8], [2, 2, 3]), 1);
+    expect(arrayCmp([0, 0, 2, 4], [0, 0, 15]), -1);
+    expect(arrayCmp([0, 13], [0, 2, 2]), 1);
+    expect(arrayCmp([11, 2], [11, 2]), 0);
+  });
+  
+  test('strcmpNaturally: compares strings naturally.', () {
+    expect(strcmpNaturally("", ""), 0);
+    expect(strcmpNaturally("2a", "10a"), -1);
+    expect(strcmpNaturally("alfa", "bravo"), -1);
+  });
+  
+  test('strStripNumbers: creates a list of integers, embedded in a string.', () {
+    expect(strStripNumbers("ab11cdd2k.144"), [11, 2, 144]);
+    expect(strStripNumbers("Ignacio Vazquez-Abrams"), []);
+  });
+  
   test('makeInitials: reduces a list of authors to a list of initials.', () {
     expect(makeInitials("John ronald reuel Tolkien"), "J.R.R.T.");
     expect(makeInitials("  e.B.Sledge "), "E.B.S.");
@@ -25,6 +54,16 @@ void main() {
     expect(makeInitials("a.s.,b.s."), "A.S.,B.S.");
     expect(makeInitials("A. Strugatsky, B...Strugatsky."), "A.S.,B.S.");
     expect(makeInitials("Иржи Кропачек, Йозеф Новотный"), "И.К.,Й.Н.");
+  });
+
+  test('Strings and lists of strings, review.', () {
+    expect(["alfa", "bravo"].join(", "), "alfa, bravo");
+    expect("щука"[0].toUpperCase(), "Щ");
+    expect('alfa, bravo'.split(','), ["alfa", " bravo"]);
+    expect("Kubler - -Ross".split(new RegExp(r'\s*(?:-\s*)+')), ["Kubler", "Ross"]);
+    expect('alfa"br"bravo"ch"charlie'
+                  .replaceAll(new RegExp(r'\"(?:\\.|[^\"\\])*\"'), " "),
+            "alfa bravo charlie");
   });
 
 }
